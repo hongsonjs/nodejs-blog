e63b7611b9257f8bd26008aa7579cd0da7923053
hongsonjs <hongson.js@gmail.com>
2023-11-24 22:55:44 +0700

    delete .prettierrc

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
-)
-app.use(express.json())
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-  })
-)
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+	'hbs',
+	handlebars({
+		extname: '.hbs',
+	})
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app)
+route(app);
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-)
+	console.log(`Example app listening at http://localhost:${port}`)
+);
