3e6b15389314543bc10249277d2a8ecc0724f3ae
hongsonjs <hongson.js@gmail.com>
2023-11-25 08:45:57 +0700

    update routes and path resourses

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.use('/search', siteController.search);
+router.get('/search', siteController.search);
 
-router.use('/', siteController.index);
+router.get('/', siteController.index);
 
 module.exports = router;
