72b542a54f495407206d77a694bda71ca2b894bc
hongsonjs <hongson.js@gmail.com>
2023-11-25 12:25:04 +0700

    update courses route, controller, model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,13 @@
 const Course = require("../models/Course")
+const { multipleMongooseToObject } = require("../../util/mongoose")
 class SiteController {
 	// [GET] /
-	async index(req, res) {
-		try {
-			const courses = await Course.find({});
-			res.json(courses);
-		} catch (error) {
-			res.status(400).json({ err: "ERROR!!!" });
-		}
+	index(req, res, next) {
+		Course.find({})
+			.then(courses => res.render('home', {
+				courses: multipleMongooseToObject(courses)
+			}))
+			.catch(next);
 	}
 	// [GET] /search
 	search(req, res) {
